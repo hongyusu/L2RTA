789ee6f86d4a57f0005a35e94d370acdb04bf5c5
hongyusu <hongyu.su.uh@gmail.com>
2015-03-10 01:02:31 +0200

    minors

--- inference_codes/batch.sh
+++ inference_codes/batch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --cpus-per-task=8
-#SBATCH --time=3-00:00:00 --mem-per-cpu=4000
+#SBATCH --cpus-per-task=1
+#SBATCH --time=0-00:00:01 --mem-per-cpu=4
 #SBATCH --array=1-1000000
 #SBATCH -p play
 export OMP_PROC_BIND=true
