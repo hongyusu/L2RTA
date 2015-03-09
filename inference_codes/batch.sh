b18a7a5b9e4cf95353071d15197411140a7dfb05
hongyusu <hongyu.su.uh@gmail.com>
2015-03-10 00:38:18 +0200

    minorssl .

--- inference_codes/batch.sh
+++ inference_codes/batch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --cpus-per-task=1
 #SBATCH --time=0:00:01 --mem-per-cpu=2
-#SBATCH --array=1-100000
+#SBATCH --array=1-100
 module load matlab
 echo $SLURM_ARRAY_TASK_ID > tmp
 #python triton_auto_run_RSTA.py $SLURM_ARRAY_TASK_ID
