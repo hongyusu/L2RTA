ed93af7ca8e21de4ab3cf64a772c0a19211783ad
hongyusu <hongyu.su.uh@gmail.com>
2015-03-10 01:09:32 +0200

    minor

--- inference_codes/triton_batch.sh
+++ inference_codes/triton_batch.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-#SBATCH --cpus-per-task=12
-#SBATCH --time=4:00:00 --mem-per-cpu=2000
+#SBATCH --cpus-per-task=1
+#SBATCH --time=0-00:00:01 --mem-per-cpu=4
 #SBATCH --array=1-100000
+#SBATCH -o hello.out
+#SBATCH -p play
+
+export OMP_PROC_BIND=true
 module load matlab
-echo $SLURM_ARRAY_TASK_ID
 python triton_auto_run_RSTA.py $SLURM_ARRAY_TASK_ID
-#matlab -nojvm -r "run_met_id($SLURM_ARRAY_TASK_ID)"
 
