2391d58b1cfc1ff728bf5ce2992f4eacb6e80741
hongyusu <hongyu.su.uh@gmail.com>
2015-03-09 23:41:12 +0200

    minors

--- inference_codes/batch.sh
+++ inference_codes/batch.sh
@@ -2,5 +2,6 @@
 #SBATCH --time=4:00:00 --mem-per-cpu=2000
 #SBATCH --array=1-616
 module load matlab
-matlab -nojvm -r "run_met_id($SLURM_ARRAY_TASK_ID)"
+echo $SLURM_ARRAY_TASK_ID
+#matlab -nojvm -r "run_met_id($SLURM_ARRAY_TASK_ID)"
 
