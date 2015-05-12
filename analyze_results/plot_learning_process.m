0d64b65b97180031b9d6b2da803b2eead4b8edb3
hongyusu <hongyu.su.uh@gmail.com>
2015-05-12 10:04:22 +0300

    minors

--- analyze_results/plot_learning_process.m
+++ analyze_results/plot_learning_process.m
@@ -6,12 +6,15 @@ function plot_learning_process(option)
 
 
 
-    dataset = {'toy10','toy50','emotions','yeast','scene'};
-    dataset = {'toy10'};
-    ts = [1 5 10 15 20 25 30];
+%     dataset = {'toy10','toy50','emotions','yeast','scene'};
+    dataset = {'toy10','toy50','emotions','yeast'};
+%     ts = [1 2 5 10 15 20 25];
+    ts = [2 5 10 20 30];
     ks = [1 2 4 6 8 10 12 14 16];
-    cs = [1 10 100];
-    gs = [1];  
+%     cs = {'0.5', '1', '10', '100'};
+    cs = {'10'};
+%     gs = {'0.1', '1', '5'};  
+    gs = {'1'};
     
     if option == 1
         plot_x_as_duality_gap(dataset,ts,ks,cs,gs)
@@ -45,25 +48,26 @@ function plot_overall_statistics(dataset,ts,ks,cs,gs)
             
             figure1 = figure('visible','off');
             set(figure1, 'PaperUnits', 'centimeters');
-            set(figure1, 'Position',[0 0 7000 2000]);
+            set(figure1, 'Position',[0 0 7000 600*length(gs)]);
 
 
-            g = gs(gi);
+            g = gs{gi};
 
             for ci = 1:length(cs)
 
                 for ti = 1:length(ts)
 
                     t = ts(ti);
-                    c = cs(ci);
+                    c = cs{ci};
 
+                    
 
                     % training_1_err, training_ham_err, obj, duality_gap, gmax-g0, update
-                    system(sprintf('for i in 1 2 4 6 8 10 12 14 16; do cat  ../outputs/backup_run/%s_tree_%d_f1_l2_k${i}_c%d_s%d_n1_RSTAs.log|tail -n3|head -n1|awk -F%s %s %s{print $6%s %s$9%s %s$11%s %s$13%s %s$14%s %s$32%s %s$33}%s|sed s/\\%%//g; done > tmp',...
+                    system(sprintf('for i in 1 2 4 6 8 10 12 14 16; do cat  ../outputs/backup_run/%s_tree_%d_f1_l2_k${i}_c%s_s%s_n1_RSTAs.log|tail -n3|head -n1|awk -F%s %s %s{print $6%s %s$9%s %s$11%s %s$13%s %s$14%s %s$32%s %s$33}%s|sed s/\\%%//g; done > tmp',...
                         dname,t,c,g,qs,qs,qs,qd,qd,qd,qd,qd,qd,qd,qd,qd,qd,qd,qd,qs));
                     x = dlmread('tmp');
                     % training_1_err, training_ham_err,test_1_err, test_ham_err, yi_tr, yi_ts
-                    system(sprintf('for i in 1 2 4 6 8 10 12 14 16; do cat  ../outputs/backup_run/%s_tree_%d_f1_l2_k${i}_c%d_s%d_n1_RSTAs.log|tail -n2|head -n1|awk -F%s %s %s{print $1%s %s$4%s %s$7%s %s$10%s %s$13%s %s$18%s %s$24}%s|sed s/://g|sed s/\\%%//g; done > tmp',...
+                    system(sprintf('for i in 1 2 4 6 8 10 12 14 16; do cat  ../outputs/backup_run/%s_tree_%d_f1_l2_k${i}_c%s_s%s_n1_RSTAs.log|tail -n2|head -n1|awk -F%s %s %s{print $1%s %s$4%s %s$7%s %s$10%s %s$13%s %s$18%s %s$24}%s|sed s/://g|sed s/\\%%//g; done > tmp',...
                         dname,t,c,g,qs,qs,qs,qd,qd,qd,qd,qd,qd,qd,qd,qd,qd,qd,qd,qs));
                     y = dlmread('tmp');
 
@@ -80,44 +84,44 @@ function plot_overall_statistics(dataset,ts,ks,cs,gs)
 
                         if ti==1
                             xlabel('K')
-                            title(sprintf('C%d', c));
+                            title(sprintf('C=%s', c));
                         end
 
                         if j==2
-                            title(sprintf('Tr 0/1 %%, C%d', c));
+                            title(sprintf('Tr 0/1 %%, C=%s', c));
                         end
                         if j==3
-                            title(sprintf('Tr hamming %%, C%d', c));
+                            title(sprintf('Tr hamming %%, C=%s', c));
                         end            
                         if j==4
-                            title(sprintf('Objective, C%d', c));
+                            title(sprintf('Objective, C=%s', c));
                         end
                         if j==5
-                            title(sprintf('Gap value, C%d', c));
+                            title(sprintf('Gap value, C=%s', c));
                         end  
                         if j==6
-                            title(sprintf('Gap %%, C%d', c));
+                            title(sprintf('Gap %%, C=%s', c));
                         end 
                         if j==7
-                            title(sprintf('Tree update %%, C%d', c));
+                            title(sprintf('Tree update %%, C=%s', c));
                         end 
                         if j==8
-                            title(sprintf('Example update %%, C%d', c));
+                            title(sprintf('Example update %%, C=%s', c));
                         end 
                         if j==9
-                            title(sprintf('Running time, C%d', c));
+                            title(sprintf('Running time, C=%s', c));
                         end 
                         if j==12
-                            title(sprintf('Ts 0/1 %%, C%d', c));
+                            title(sprintf('Ts 0/1 %%, C=%s', c));
                         end
                         if j==13
-                            title(sprintf('Ts hamming %%, C%d', c));
+                            title(sprintf('Ts hamming %%, C=%s', c));
                         end
                         if j==14
-                            title(sprintf('Yi tr %%, C%d', c));
+                            title(sprintf('Yi tr %%, C=%s', c));
                         end
                         if j==15
-                            title(sprintf('Yi ts %%, C%d', c));
+                            title(sprintf('Yi ts %%, C=%s', c));
                         end
                         hold off
                     end
@@ -128,7 +132,7 @@ function plot_overall_statistics(dataset,ts,ks,cs,gs)
 
             end
             
-            export_fig(sprintf('../plots/plot_overall_statistics_%s_%d.jpg',dataset{di},gs(gi)),figure1)
+            export_fig(sprintf('../plots/plot_overall_statistics_%s_g%s.jpg',dataset{di},gs{gi}),figure1)
 
         end
 
@@ -160,8 +164,8 @@ function plot_x_as_iteration_count(dataset,ts,ks,cs,gs)
                     for ki = 1:length(ks)
 
                         % training_1_err, training_ham_err, obj, duality_gap, gmax-g0, update
-                        system(sprintf('cat  ../outputs/backup_run/%s_tree_%d_f1_l2_k%d_c%d_s%d_n1_RSTAs.log|head -n20|awk -F%s %s %s{print $1%s %s$6%s %s$9%s %s$11%s %s$13%s %s$14%s %s$32%s %s$33}%s|sed s/://g|sed s/\\%%//g > tmp',...
-                            dataset{di},ts(ti),ks(ki),cs(ci),gs(gi),qs,qs,qs,qd,qd,qd,qd,qd,qd,qd,qd,qd,qd,qd,qd,qd,qd,qs));
+                        system(sprintf('cat  ../outputs/backup_run/%s_tree_%d_f1_l2_k%d_c%s_s%s_n1_RSTAs.log|head -n20|awk -F%s %s %s{print $1%s %s$6%s %s$9%s %s$11%s %s$13%s %s$14%s %s$32%s %s$33}%s|sed s/://g|sed s/\\%%//g > tmp',...
+                            dataset{di},ts(ti),ks(ki),cs{ci},gs{gi},qs,qs,qs,qd,qd,qd,qd,qd,qd,qd,qd,qd,qd,qd,qd,qd,qd,qs));
                         
                         x = dlmread('tmp');
                         
@@ -173,28 +177,28 @@ function plot_x_as_iteration_count(dataset,ts,ks,cs,gs)
                             plot(x(:,j),'-');
                             xlabel('Iteration count');
                             if j==1
-                                title(sprintf('Running time, K=%d, C=%d',ks(ki),cs(ci)));
+                                title(sprintf('Running time, K=%d, C=%s',ks(ki),cs{ci}));
                             end
                             if j==2
-                                title(sprintf('0/1 loss tr, K=%d, C=%d',ks(ki),cs(ci)));
+                                title(sprintf('0/1 loss tr, K=%d, C=%s',ks(ki),cs{ci}));
                             end
                             if j==3
-                                title(sprintf('Hamming loss tr, K=%d, C=%d',ks(ki),cs(ci)));
+                                title(sprintf('Hamming loss tr, K=%d, C=%s',ks(ki),cs{ci}));
                             end
                             if j==4
-                                title(sprintf('Objective, K=%d, C=%d',ks(ki),cs(ci)));
+                                title(sprintf('Objective, K=%d, C=%s',ks(ki),cs{ci}));
                             end
                             if j==5
-                                title(sprintf('Gap value, K=%d, C=%d',ks(ki),cs(ci)));
+                                title(sprintf('Gap value, K=%d, C=%s',ks(ki),cs{ci}));
                             end
                             if j==6
-                                title(sprintf('Gap %, K=%d, C=%d',ks(ki),cs(ci)));
+                                title(sprintf('Gap %%, K=%d, C=%s',ks(ki),cs{ci}));
                             end
                             if j==7
-                                title(sprintf('Tree update, K=%d, C=%d',ks(ki),cs(ci)));
+                                title(sprintf('Tree update, K=%d, C=%s',ks(ki),cs{ci}));
                             end
                             if j==8
-                                title(sprintf('Example update, K=%d, C=%d',ks(ki),cs(ci)));
+                                title(sprintf('Example update, K=%d, C=%s',ks(ki),cs{ci}));
                             end
                             hold off
                         end
@@ -203,7 +207,7 @@ function plot_x_as_iteration_count(dataset,ts,ks,cs,gs)
                 lgd = legend(strcat('T=',num2str(ts')));
                 set(lgd,  'fontsize', 10, 'interpreter','latex','Position', [0.01,0.2,0.1,0.1]);
 
-                export_fig(sprintf('../plots/plot_learning_process_%s_%d_%d.jpg',dataset{di},gs(gi),cs(ci)),figure1)
+                export_fig(sprintf('../plots/plot_learning_process_%s_g%s_c%s.jpg',dataset{di},gs{gi},cs{ci}),figure1)
             end
             
 
@@ -237,8 +241,8 @@ function plot_x_as_duality_gap(dataset,ts,ks,cs,gs)
                     for ki = 1:length(ks)
 
                         % training_1_err, training_ham_err, obj, duality_gap, gmax-g0, update
-                        system(sprintf('cat  ../outputs/backup_run/%s_tree_%d_f1_l2_k%d_c%d_s%d_n1_RSTAs.log|head -n20|awk -F%s %s %s{print $6%s %s$9%s %s$11%s %s$13%s %s$14%s %s$32%s %s$33}%s|sed s/\\%%//g > tmp',...
-                            dataset{di},ts(ti),ks(ki),cs(ci),gs(gi),qs,qs,qs,qd,qd,qd,qd,qd,qd,qd,qd,qd,qd,qd,qd,qs));
+                        system(sprintf('cat  ../outputs/backup_run/%s_tree_%d_f1_l2_k%d_C=%s_s%d_n1_RSTAs.log|head -n20|awk -F%s %s %s{print $6%s %s$9%s %s$11%s %s$13%s %s$14%s %s$32%s %s$33}%s|sed s/\\%%//g > tmp',...
+                            dataset{di},ts(ti),ks(ki),cs{ci},gs(gi),qs,qs,qs,qd,qd,qd,qd,qd,qd,qd,qd,qd,qd,qd,qd,qs));
                         
                         
                         x = dlmread('tmp');
@@ -251,7 +255,7 @@ function plot_x_as_duality_gap(dataset,ts,ks,cs,gs)
                             subplot(length(ks),length(js),j+(ki-1)*length(js))
                             hold on
                             plot(x(:,5),x(:,js(j)),'-')
-                            title(sprintf('T=%d, C=%d',ks(ki),cs(ci)));
+                            title(sprintf('T=%d, C=%s',ks(ki),cs{ci}));
 
                             xlabel('Duality gap percentage')
                             
@@ -280,7 +284,7 @@ function plot_x_as_duality_gap(dataset,ts,ks,cs,gs)
                 lgd = legend(strcat('K=',num2str(ts')));
                 set(lgd,  'fontsize', 10, 'interpreter','latex','Position', [0,0.2,0.1,0.1]);
 
-                export_fig(sprintf('plot_learning_process_%s_%d_%d.jpg',dataset{di},gs(gi),cs(ci)),figure1)
+                export_fig(sprintf('plot_learning_process_%s_%d_%d.jpg',dataset{di},gs(gi),cs{ci}),figure1)
             end
             
 
