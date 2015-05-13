0b895010f20018c1ae080d92603164113b45e40e
hongyusu <hongyu.su.uh@gmail.com>
2015-05-13 13:36:19 +0300

    automatic commit and submit minor revisions

--- inference_codes/auto_run_RSTA.py
+++ inference_codes/auto_run_RSTA.py
@@ -102,25 +102,21 @@ def run():
   is_main_run_factor=5
   #filenames=['toy10','toy50','emotions','medical','enron','yeast','scene','cal500','fp','cancer']
   #filenames=['cancer']
-  filenames=['toy10','toy50','emotions','yeast','scene','fp']
+  filenames=['toy10','toy50','emotions','fp','yeast','scene']
   n=0
   # generate job_queue
   logging.info('\t\tGenerating priority queue.')
   for newton_method in ['1','0']:
     for filename in filenames:
-      #for slack_c in ['1','100','0.1','10','0.01','50','0.5','20','0.05','5']:
-      #for slack_c in ['1','100','0.1','10','0.01']:
-      for slack_c in ['1', '10', '100', '0.01','0.5', '0.1']:
+      for slack_c in ['1', '10', '100', '0.01', '0.1']:
         for t in [1, 2, 5, 10, 20, 30]:
           para_t="%d" % (t)
           graph_type = 'tree'
           for kappa in ['1','2','4','6','8','10','12','14','16']:
-          #for kappa in ['2']:
             for l_norm in ['2']:
               #for kth_fold in ['1','2','3','4','5']:
               for kth_fold in ['1']:
-                #for loss_scaling_factor in ['0.5','0.1','1','5','10']:
-                for loss_scaling_factor in ['0.1','1','5']:
+                for loss_scaling_factor in ['0.1','1','10']:
                   if checkfile(filename,graph_type,para_t,kth_fold,l_norm,kappa,slack_c,loss_scaling_factor,newton_method):
                     continue
                   else:
